5aa813a57ebc402c064aea382533109634759b5a
Rishav Dutta Manik <rishav.duttamanik2020@gmail.com>
2026-05-21 01:40:31 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,14 +2,14 @@
 import pytest
 from src.app import add, divide
 
-# 1. Successful Test Case
-def test_add_success():
-    assert add(2, 3) == 5
+# # 1. Successful Test Case
+# def test_add_success():
+#     assert add(2, 3) == 5
 
-# 2. Successful Test Case (Exception handling)
-def test_divide_by_zero_success():
-    with pytest.raises(ValueError):
-        divide(10, 0)
+# # 2. Successful Test Case (Exception handling)
+# def test_divide_by_zero_success():
+#     with pytest.raises(ValueError):
+#         divide(10, 0)
 
 # 3. Intentionally Failed Test Case (For demonstration)
 def test_add_failure():
